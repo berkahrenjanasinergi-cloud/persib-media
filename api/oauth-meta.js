60897d9b25f842fe90d9a4b724685494658822c1
berkahrenjanasinergi-cloud <berkahrenjanasinergi@gmail.com>
2026-08-28 16:30:51 +0700

    Update oauth-meta.js

--- api/oauth-meta.js
+++ api/oauth-meta.js
@@ -2,6 +2,6 @@ export default function handler(req,res){
  const A=process.env.META_APP_ID;
  if(!A)return res.status(500).json({error:"META_APP_ID belum diset di env Vercel"});
  const redirect=encodeURIComponent("https://"+req.headers.host+"/api/oauth-meta-callback");
- const scope=encodeURIComponent("pages_show_list,pages_manage_posts,pages_read_engagement,instagram_basic,instagram_content_publish");
+ const scope=encodeURIComponent("pages_show_list,pages_manage_posts,pages_read_engagement,instagram_basic,instagram_content_publish,business_management");
  res.redirect("https://www.facebook.com/v21.0/dialog/oauth?client_id="+A+"&redirect_uri="+redirect+"&scope="+scope);
 }
