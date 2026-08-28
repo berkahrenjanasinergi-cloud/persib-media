95491418ce838faf392f17fa28c48aae56ab67d3
berkahrenjanasinergi-cloud <berkahrenjanasinergi@gmail.com>
2026-08-28 17:19:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.CONFIG = {
   supabaseUrl: "https://wnlmqnehlixjucbdxcwc.supabase.co",
   supabaseAnonKey: "sb_publishable_FpPpGo_RbQSgihKSww7h-w_SO7J8ErS",
   theSportsDBKey: "3",
-  geminiKey: "AQ.Ab8RN6K8HpQhrkHtGE_1geslTNQJ55LfSvnmr8Ci-wHBbBUgBw",
+  geminiKey: "",
   groqKey: "",
   telegramBotToken: "",
   telegramChatId: "",
