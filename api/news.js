a76c7c02860b32a0dfd1d18e6b64dfcf276e44ca
berkahrenjanasinergi-cloud <berkahrenjanasinergi@gmail.com>
2026-09-01 10:39:33 +0700

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,18 +1,29 @@
 const FEEDS=[
- {name:"Persib.co.id",tier:1,url:"https://persib.co.id/feed",rel:97},
+ {name:"Persib.co.id",tier:1,url:"https://news.google.com/rss/search?q=persib+site:persib.co.id&hl=id&gl=ID&ceid=ID:id",rel:97,ps:1},
  {name:"Simamaung",tier:2,url:"https://simamaung.com/feed",rel:92},
  {name:"Bandung Football",tier:2,url:"https://bandungfootball.com/feed",rel:88},
+ {name:"GNews Persib",tier:2,url:"https://news.google.com/rss/search?q=persib+bandung&hl=id&gl=ID&ceid=ID:id",rel:90,ps:1},
+ {name:"GNews Bobotoh",tier:2,url:"https://news.google.com/rss/search?q=bobotoh&hl=id&gl=ID&ceid=ID:id",rel:88,ps:1},
+ {name:"GNews Viking",tier:2,url:"https://news.google.com/rss/search?q=viking+persib&hl=id&gl=ID&ceid=ID:id",rel:84,ps:1},
+ {name:"GNews Persib Tribun",tier:2,url:"https://news.google.com/rss/search?q=persib+site:tribunnews.com&hl=id&gl=ID&ceid=ID:id",rel:86,ps:1},
+ {name:"GNews Persib Kompas",tier:2,url:"https://news.google.com/rss/search?q=persib+site:kompas.com&hl=id&gl=ID&ceid=ID:id",rel:86,ps:1},
+ {name:"GNews Persib CNN",tier:2,url:"https://news.google.com/rss/search?q=persib+site:cnnindonesia.com&hl=id&gl=ID&ceid=ID:id",rel:85,ps:1},
+ {name:"GNews Persib Liputan6",tier:2,url:"https://news.google.com/rss/search?q=persib+site:liputan6.com&hl=id&gl=ID&ceid=ID:id",rel:84,ps:1},
+ {name:"GNews Persib Bola.net",tier:2,url:"https://news.google.com/rss/search?q=persib+site:bola.net&hl=id&gl=ID&ceid=ID:id",rel:84,ps:1},
+ {name:"GNews Bandung",tier:2,url:"https://news.google.com/rss/search?q=bandung&hl=id&gl=ID&ceid=ID:id",rel:80,ps:1,f:/persib|bobotoh|bandung|jabar|maung/i},
  {name:"Pikiran Rakyat",tier:2,url:"https://www.pikiran-rakyat.com/feed",rel:84,f:/persib|bobotoh|bandung|jabar/i},
  {name:"Bola.com",tier:2,url:"https://www.bola.com/feed",rel:86,f:/persib|bobotoh|liga 1|timnas|sepak ?bola|maung|piala/i},
  {name:"Okezone",tier:2,url:"https://rss.okezone.com/rss/sport",rel:82,f:/persib|bobotoh|timnas|liga/i},
  {name:"Detik Sport",tier:2,url:"https://feed.detik.com/index.php/sport",rel:88,f:/persib|bobotoh|liga|timnas|sepak ?bola|piala/i},
  {name:"Tribun Jabar",tier:2,url:"https://jabar.tribunnews.com/rss",rel:85,f:/bandung|jabar|persib|bobotoh/i},
- {name:"GNews Persib",tier:2,url:"https://news.google.com/rss/search?q=persib+bandung&hl=id&gl=ID&ceid=ID:id",rel:90,ps:1},
- {name:"GNews Bobotoh",tier:2,url:"https://news.google.com/rss/search?q=bobotoh&hl=id&gl=ID&ceid=ID:id",rel:88,ps:1},
+ {name:"ANTARA",tier:2,url:"https://www.antaranews.com/rss/terkini",rel:90,f:/bandung|jabar|persib|bobotoh|timnas|sepak ?bola/i},
  {name:"GNews Liga 1",tier:2,url:"https://news.google.com/rss/search?q=liga+1+indonesia&hl=id&gl=ID&ceid=ID:id",rel:88,ps:1},
- {name:"GNews Bola Dunia",tier:2,url:"https://news.google.com/rss/search?q=sepak+bola+dunia&hl=id&gl=ID&ceid=ID:id",rel:85,ps:1},
+ {name:"GNews Timnas",tier:2,url:"https://news.google.com/rss/search?q=timnas+indonesia&hl=id&gl=ID&ceid=ID:id",rel:86,ps:1},
+ {name:"GNews Champions",tier:2,url:"https://news.google.com/rss/search?q=champions+league&hl=id&gl=ID&ceid=ID:id",rel:85,ps:1},
+ {name:"GNews Premier",tier:2,url:"https://news.google.com/rss/search?q=premier+league&hl=id&gl=ID&ceid=ID:id",rel:85,ps:1},
  {name:"BBC Football",tier:2,url:"https://feeds.bbci.co.uk/sport/football/rss.xml",rel:95},
- {name:"Guardian Football",tier:2,url:"https://www.theguardian.com/football/rss",rel:93}
+ {name:"Guardian Football",tier:2,url:"https://www.theguardian.com/football/rss",rel:93},
+ {name:"Sky Sports",tier:2,url:"https://www.skysports.com/rss/12040",rel:90}
 ];
 export default async function handler(req,res){
  const out=[];
